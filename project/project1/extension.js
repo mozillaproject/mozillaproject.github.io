1ccc5f6f3648b5bd48d5fe8cd733ca570ba72390
mozillaproject <119520847+mozillaproject@users.noreply.github.com>
2022-12-02 18:09:27 -0500

    Update extension.js

--- project/project1/extension.js
+++ project/project1/extension.js
@@ -1,17 +1,8 @@
 document.body.style.border="10px solid purple"
 
-
-    function changeFont(font) {
-        var sel = window.getSelection(); // Gets selection
-        if (sel.rangeCount) {
-          // Creates a new element, and insert the selected text with the chosen font inside
-          var e = document.createElement('span');
-          e.style = 'font-family:' + font.value + ';'; 
-          e.innerHTML = sel.toString();
-      
-          // https://developer.mozilla.org/en-US/docs/Web/API/Selection/getRangeAt
-          var range = sel.getRangeAt(0);
-          range.deleteContents(); // Deletes selected text…
-          range.insertNode(e); // … and inserts the new element at its place
-        }
-      }
+ document.getElementById("btnCopy").addEventListener("click", function(){
+   var copyText = document.getElementById("myText");
+   copyText.select();
+   document.execCommand("Copy");
+   alert("Copied to clipboard!");
+ });
