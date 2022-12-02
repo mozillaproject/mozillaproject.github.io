275dcc87dc0e39eac6dcf509e4469c0f84d97785
mozillaproject <119520847+mozillaproject@users.noreply.github.com>
2022-12-02 17:23:21 -0500

    Add files via upload

---
+++ project/project1/extension.js
@@ -0,0 +1,17 @@
+document.body.style.border="10px solid purple"
+
+
+    function changeFont(font) {
+        var sel = window.getSelection(); // Gets selection
+        if (sel.rangeCount) {
+          // Creates a new element, and insert the selected text with the chosen font inside
+          var e = document.createElement('span');
+          e.style = 'font-family:' + font.value + ';'; 
+          e.innerHTML = sel.toString();
+      
+          // https://developer.mozilla.org/en-US/docs/Web/API/Selection/getRangeAt
+          var range = sel.getRangeAt(0);
+          range.deleteContents(); // Deletes selected text…
+          range.insertNode(e); // … and inserts the new element at its place
+        }
+      }
