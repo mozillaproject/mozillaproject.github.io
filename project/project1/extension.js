2c46f4d0adca2b47d8543002e9b93931d3377f25
mozillaproject <119520847+mozillaproject@users.noreply.github.com>
2022-12-02 21:34:16 -0500

    Delete extension.js

--- project/project1/extension.js
+++
@@ -1,8 +0,0 @@
-document.body.style.border="10px solid purple"
-
- document.getElementById("btnCopy").addEventListener("click", function(){
-   var copyText = document.getElementById("myText");
-   copyText.select();
-   document.execCommand("Copy");
-   alert("Copied to clipboard!");
- });
